1dda6808f89c58072cb1c633bb690f41f0647974
ceyhunbcelik <ceyhunbcelik@icloud.com>
2021-05-03 01:41:07 +0300

    fix1

--- App/www/Containers/DropDown.php
+++ App/www/Containers/DropDown.php
@@ -11,4 +11,28 @@
 <p class='title county'>İLÇE</p>
 <select id="county"></select>
 
-<script src="<?= js('county') ?>"></script>
+<script>
+$(document).ready(function(){
+
+    $('#city').change(function(){
+        
+        var city_name = $(this).val()
+        var url = '<?= URL . '/ajax/county' ?>'
+
+        $.ajax({
+            type : 'POST',
+            url  : url,
+            data : {
+                'city_name' : city_name,
+            },
+            success: function(e){
+                $('.county').show()
+                $('#county').show()
+                
+                $('#county').html(e);
+            }
+        })
+    })
+
+})
+</script>
