c7d606631dfea6014c692e23e89dbe1271ddfeb7
Ceyhun Bahadır Çelik <41891940+ceyhunbcelik@users.noreply.github.com>
2021-06-18 10:57:43 +0300

    Update Route.php

--- App/Classes/Route.php
+++ App/Classes/Route.php
@@ -8,52 +8,48 @@ class Route{
 
   public static function run($url, $callback, $method = 'GET', $session){
 
-    $request_uri = self::parse_url();
+    $patterns = [
+      '{param}' => '([0-9a-zA-Z-]+)',
+      '{id}'  => '([0-9]+)'
+    ];
 
-    echo $request_uri . ' - ' . $url . '<br>';
+    $parse_url = self::parse_url();
+    $url       = str_replace(array_keys($patterns), array_values($patterns), $url);
 
-    if($request_uri == $url){
-      $session = explode('|', $session);
-      intval($session[0]) ? Session::In($session[1]) : Session::Out($session[1]);
-    }
+    if(preg_match('@^' . $url . '$@', $parse_url, $parameters)){
+      
+      unset($parameters[0]);
 
-    $method = explode('|', strtoupper($method));
+      $session    = explode('|', $session);
+      $session[0] = intval($session[0]);
+      $session[0] ? Session::In($session[1]) : Session::Out($session[1]);
 
-    if(in_array($_SERVER['REQUEST_METHOD'], $method)){
+      $method = explode('|', strtoupper($method));
+      if(!in_array($_SERVER['REQUEST_METHOD'], $method)) exit;
 
-      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        Session::Token();
+      if($_SERVER['REQUEST_METHOD'] == 'POST') Session::Token();
+
+      if(is_callable($callback)){
+        call_user_func_array($callback, $parameters);
+        exit;
+      }
+      
+      $controller = explode('@', $callback);
+
+      $className = explode('/', $controller[0]);
+      $className = end($className);
+
+      $controllerFile = PATH . '/App/Subdomains/' . SUBDOMAIN . '/Controllers/' . $controller[0] . '.php';
+
+      if(file_exists($controllerFile)){
+        require_once($controllerFile);
+
+        call_user_func_array([new $className, $controller[1]], $parameters);
+        exit;
       }
 
-      $patterns = [
-        '{param}' => '([0-9a-zA-Z]+)',
-        '{id}'  => '([0-9]+)'
-      ];
-      
-      $url = str_replace(array_keys($patterns), array_values($patterns), $url);
-      
-      if(preg_match('@^' . $url . '$@', $request_uri, $parameters)){
-        unset($parameters[0]);
-
-        if(is_callable($callback)){
-          call_user_func_array($callback, $parameters);
-          exit;
-        }
-
-        $controller = explode('@', $callback);
-
-        $className = explode('/', $controller[0]);
-        $className = end($className);
-
-        $controllerFile = PATH . '/App/Subdomains/' . SUBDOMAIN . '/Controllers/' . $controller[0] . '.php';
-
-        if(file_exists($controllerFile)){
-          require_once($controllerFile);
-          
-          call_user_func_array([new $className, $controller[1]], $parameters);
-          exit;
-        }
-      } 
     }
+    
   }
+  
 }
