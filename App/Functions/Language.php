7659c7116949b12006f1bad731b718273a9b4be2
ceyhunbcelik <ceyhunbcelik@icloud.com>
2021-06-19 17:58:16 +0300

    language optimization

--- App/Functions/Language.php
+++ App/Functions/Language.php
@@ -1,24 +1,16 @@
 <?php
 
-$lang_lib = [
-  'tr' => [
-    'title'  => 'Türkçe',
-    'source' => require_once(PATH . '/App/Module/Language/tr.php')
-  ],
-  'en' => [
-    'title'  => 'English',
-    'source' => require_once(PATH . '/App/Module/Language/en.php')
-  ]
-];
+$available_languages = ['tr', 'en'];
+$language_library = require_once(PATH . '/App/Module/Language/language_library.php');
 
 function language(){
 
   if(isset($_GET['lang'])){
   
-    global $lang_lib;
+    global $available_languages;
 
     $get = mb_strtolower($_GET['lang']);
-    $lang = in_array($get, array_keys($lang_lib)) ? $get : 'tr';
+    $lang = in_array($get, $available_languages) ? $get : 'tr';
 
     $_SESSION['LANGUAGE'] = $lang;
   }
@@ -31,11 +23,11 @@ function language(){
 }
 
 function translate($param, $lang = NULL){
-
-  global $lang_lib;
+  
+  global $language_library;
 
   $lang = is_null($lang) ? language() : $lang;
 
-  return $lang_lib[language()]['source'][$param];
+  return isset($language_library[$param]) ? $language_library[$param][$lang] : $language_library['invalid_translate'][$lang];
 
 }
---
+++ App/Module/Language/language_library.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'invalid_translate' => [
+        'tr' => 'Geçersiz Çeviri',
+        'en' => 'Invalid Translate'
+    ],
+    'login_page' => [
+        'tr' => 'Giriş Sayfası',
+        'en' => 'Login Page'
+    ]
+];
